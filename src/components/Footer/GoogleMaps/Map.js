17c459de08aff28b5badd78ab65289109963783a
Bruno Malagoli <bmalagoli2@gmail.com>
2022-01-04 19:59:21 -0300

    Cambios de seguridad

---
+++ src/components/Footer/GoogleMaps/Map.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+const MapContainer = () => {
+  const apiKey = `${process.env.REACT_APP_MAPS_API_KEY}`
+  const mapStyles = {   
+    position: "relative",
+    top: "5%",     
+    height: "90%",
+    width: "30%"}
+    const locations = [
+        {
+        name: "location1",
+        location:{
+            lat: -34.58818014285715,
+            lng: -58.5097898367347
+        }
+        }
+    ]
+  
+  const defaultCenter = {
+    lat: -34.58818014285715 , lng: -58.5097898367347
+  }
+  
+  return (
+     <LoadScript
+       googleMapsApiKey={apiKey}>
+        <GoogleMap
+          mapContainerStyle={mapStyles}
+          zoom={13}
+          center={defaultCenter}>
+              {
+                  locations.map(item => {
+                    return (
+                    <Marker key={item.name} position={item.location}/>
+                    )
+                  })
+              }
+          </GoogleMap>
+     </LoadScript>
+  )
+}
+export default MapContainer;
